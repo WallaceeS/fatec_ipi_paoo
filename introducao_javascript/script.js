c12240c5b5f1eaae2f465c5640699ee79800579c
Wallace Souza <wallacesilvadesouzaa@outlook.com>
2026-02-25 18:11:09 -0300

    learn: encerra codigo 1.4.1

--- introducao_javascript/script.js
+++ introducao_javascript/script.js
@@ -1,1 +1,77 @@
-console.log('Hello, JS!')
+//comparação por igualdade
+
+//Java: ==, Python: ==
+//Javascript: == ou ===(usamos apenas esse)
+
+// console.log(true == 1): true
+// console.log(1 === '1'): false
+// console.log(1 == '1'): true
+
+
+//coerção explicita
+// const n1 = 2
+// const n2 = '3'
+// const n3 = n1 + Number(n2)
+// console.log(`${n1} + ${n2} = ${n3}`)
+
+//coerção implicita
+
+// const n1 = 2
+// const n2 = '3'
+// const n3 = n1 + n2
+
+// console.log(`${n1} + ${n2} = ${n3}`)
+
+//tipos
+//js é dinamicamente tipa
+
+// let a = 2
+// console.log(typeof(a))
+// a = true
+// console.log(typeof(a))
+
+// s = "abc"
+// String s = "abc"
+
+// listaDeLista[[],[],[]]
+
+
+//declaração de variaveis 
+//const, let e var
+
+// var idade = 18
+// console.log(`Oi, ${nome}`)
+// if(idade >= 18){
+//     var nome = 'Maria'
+//     console.log(`Parabens,${nome}. Você pode dirigir`)
+// }
+// console.log("Até mais, " + nome)
+// var linguagem = 'Javascript'
+// console.log(`Aprendendo ${linguagem}`)
+// var linguagem = 'Java'
+// console.log('Aprendendo' + linguagem)
+
+
+// console.log('Antes do for')
+// for (const i = 0; i < 10; i = i +1){
+//     console.log('for com const...')
+// }
+// console.log('Depois do for')
+
+
+
+// let nome = 'Maria'
+// console.log(nome)
+// nome = `Meu nome é ${nome}.`
+// console.log(nome)
+
+
+
+
+// const nome = 'José'
+// console.log(nome)
+// nome = 'José Silva'
+// console.log(nome)
+
+
+// console.log('Hello, JS!')
